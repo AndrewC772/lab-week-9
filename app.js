44a30b46c97e5e04f02e69c851be6ac7aec0ed6e
Andrew <chen.andrew772@gmail.com>
2021-03-09 23:47:16 -0800

    added index.html and style.css along with app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+let number1 = 22;
+const refnumber = 22;
+const randomstring = "abcdef";
+let booleantest = true;
+
+if (number1 === refnumber) {
+    console.log("they match")
+} else {
+    console.log("no luck")
+}
+
+for (letter of randomstring) {
+    console.log(letter)
+}
